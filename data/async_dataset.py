f300bc9efcbeb2b48f3cb35631aaa157209c1942
ThatMatin <matin.ahmadi.2011@gmail.com>
2024-07-16 18:25:37 +0330

    ignore all-zeros data points

--- data/async_dataset.py
+++ data/async_dataset.py
@@ -48,7 +48,9 @@ class AsyncDataset(IterableDataset):
                 for i in range(tensors.get_batch_size()):
                     if self.__stop_event.is_set():
                         break
-                    self.put(tensors[i])
+                    # TODO: There are all-zeros entries in the data. Check where they occur
+                    if torch.any(tensors[i][0]).item() and torch.any(tensors[i][1]).item():
+                        self.put(tensors[i])
 
         except Exception as e:
             logger.error(f"{traceback.format_exc()}\n{e}")
